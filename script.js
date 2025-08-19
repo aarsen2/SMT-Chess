bbb4e9d556a437d42b4bccdd36ce18d345233599
Aaron Hansen <ahanse29@wgu.edu>
2025-08-19 12:38:56 -0600

    breaks lots of stuff, but it does separate things

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let chessBoard = document.getElementById("chessBoard")
+import { createChessBoard } from "/display.js";
+
+
+createChessBoard()
+
+
+
+/*
 let boardArray = Array(8).fill(null).map(() => Array(8).fill(null));;
-let boardSize = 700;
-let color1 = "white"
-let color2 = "DarkGray"
-let highlightColor = "gray"
-let hoverColor = "LightGray"
-let oldHoverColor = "";
+
 let selected = false;
 let selectedPiece = [0,0];
 let lastSelectedPiece = [0,0];
@@ -14,8 +16,7 @@ let logging = true;
 let player1 = "Aaron"
 let player2 = "Ashton"
 let turn = player1;
-let logBox = document.getElementById("logBox")
-let turnDisplay = document.getElementById("turnDisplay")
+
 
 
 
@@ -24,60 +25,6 @@ function changeTurn() {
     if (turn == player1) {turn = player2}
     else {turn = player1}
 }
-
-function createChessBoard() {
-    chessBoard.style.height = boardSize + "px";
-    chessBoard.style.width = boardSize + "px";
-    chessBoard.style.borderColor = "black"
-    chessBoard.style.borderStyle = "solid"
-    chessBoard.style.borderWidth = "1"
-    chessBoard.style.display = "grid";
-    chessBoard.style.gridTemplateColumns = "repeat(8, 1fr)";
-    chessBoard.style.gridTemplateRows = "repeat(8, 1fr)";
-
-    
-    //Cell Creation
-    for (let row = 0; row < 8; row++) {
-        let newthing = 9;
-        for (let column = 0; column < 8; column++) {
-            //new Cell Element Creation with ID's j = collumn; i = row.
-            let cell = document.createElement("div")
-            cell.id = `${column},${row}`
-            chessBoard.appendChild(cell)
-
-
-            //style cells
-            
-            boardArray[column][row] = cell
-            cell.style.boxSizing = "border-box"; 
-            cell.style.height = boardSize/8 + "px";
-            cell.style.width = boardSize/8 + "px";
-            cell.style.backgroundColor = (row + column) % 2 == 0 ? color1 : color2
-            cell.style.alignItems = "center"
-            cell.style.justifyContent = "center"
-            cell.style.display = "flex"
-        
-
-
-
-            //Adding an event for each cell
-            cell.addEventListener("click", cellClickHandler)
-            cell.addEventListener("mouseover", function() {
-                    if (this.style.backgroundColor != "gray") {
-                        this.style.backgroundColor = "lightGray";
-                    }
-                    console.log("mouseover")
-                }
-            )
-            cell.addEventListener("mouseout", function() {
-                    if (this.style.backgroundColor != "gray") {
-                        this.style.backgroundColor = (row + column) % 2 == 0 ? color1 : color2
-                    }
-                    console.log("mouseout") 
-                }
-            )
-        }
-    }
 
 
     
@@ -285,3 +232,5 @@ function movePiece() {
 
 createChessBoard(boardSize);
 initializeBoard()
+
+*/
